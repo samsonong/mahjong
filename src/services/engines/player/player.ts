c6a64ebf69521efec5619543815683891c421923
Samson Ong <samson.ong@fundnel.com>
2024-09-24 17:09:13 +0800

    Update hand types
    Open hands will be stored as sets, together with pre-computed scores
    
    Closed hands will remain using `SORTED_TILES`

--- src/services/engines/player/player.ts
+++ src/services/engines/player/player.ts
@@ -1,12 +1,20 @@
 import { SORTED_TILES } from "../tiles/sortTiles";
+import { TILE_TYPE } from "../tiles/tiles";
 
 export type HAND = {
-  open: SORTED_TILES[];
-  close: SORTED_TILES[];
+  open: OPEN_HAND;
+  closed: CLOSED_HAND;
 };
 
+export type CLOSED_HAND = SORTED_TILES[];
+
+export type OPEN_HAND = {
+  tiles: TILE_TYPE[];
+  score: number;
+}[];
+
 export class Player {
-  hand: HAND = { open: [], close: [] };
+  hand: HAND = { open: [], closed: [] };
 
   wallet: number;
 
