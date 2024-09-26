39aca263beac53dd7b090aa691d98dcbd9a20288
Samson Ong <contact@samsonong.com>
2024-09-26 22:56:26 +0800

    Implement `thirteenWonders`

--- src/services/engines/scoring/checkHand.ts
+++ src/services/engines/scoring/checkHand.ts
@@ -9,3 +9,48 @@ export default function (hand: HAND) {
   const { open, closed } = hand;
   const fullHand: SORTED_TILES[] = mergeSortedTilesArray(open, closed);
 }
+
+function thirteenWonders(hand: SORTED_TILES[]): boolean {
+  /**
+   * Criteria:
+   *
+   * 1. Have 1 of all honor tiles & terminal number tiles (1/9 of bamboo/circle/myriad)
+   * 2. Exactly one pair
+   *
+   * Early escape:
+   *
+   * a. One of (1) has >2 tiles
+   * b. More than one pair
+   */
+  const tiles: TILES[] = [
+    NUMBER_TILES.BAMBOO_ONE,
+    NUMBER_TILES.BAMBOO_NINE,
+    NUMBER_TILES.CIRCLE_ONE,
+    NUMBER_TILES.CIRCLE_NINE,
+    NUMBER_TILES.MYRIAD_ONE,
+    NUMBER_TILES.MYRIAD_NINE,
+    HONOR_TILES.WIND_NORTH,
+    HONOR_TILES.WIND_SOUTH,
+    HONOR_TILES.WIND_EAST,
+    HONOR_TILES.WIND_WEST,
+    HONOR_TILES.DRAGON_RED,
+    HONOR_TILES.DRAGON_GREEN,
+    HONOR_TILES.DRAGON_WHITE,
+  ];
+  let hasPair: boolean = false;
+  for (let i = 0; i < tiles.length; i++) {
+    const thisTile = tiles[i];
+
+    const matchingTiles = findTile(hand, thisTile);
+    if (matchingTiles.length === 1) continue;
+    if (matchingTiles.length === 2) {
+      if (hasPair) return false;
+
+      hasPair = true;
+      continue;
+    }
+
+    return false;
+  }
+  return hasPair;
+}
