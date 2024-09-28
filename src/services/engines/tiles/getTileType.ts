4da06c172bfd8f2b51c937327b8276290770aa92
Samson Ong <contact@samsonong.com>
2024-09-28 20:27:15 +0800

    Implement `getTileType`

---
+++ src/services/engines/tiles/getTileType.ts
@@ -0,0 +1,17 @@
+import { TILES, NUMBER_TILES, HONOR_TILES, BONUS_TILES } from "./tiles";
+
+export default function (
+  tile: TILES
+): "NUMBER_TILES" | "HONOR_TILES" | "BONUS_TILES" {
+  if (Object.values(NUMBER_TILES).includes(tile as NUMBER_TILES)) {
+    return "NUMBER_TILES";
+  }
+  if (Object.values(HONOR_TILES).includes(tile as HONOR_TILES)) {
+    return "HONOR_TILES";
+  }
+  if (Object.values(BONUS_TILES).includes(tile as BONUS_TILES)) {
+    return "BONUS_TILES";
+  }
+
+  throw new Error("Phantom tile?");
+}
