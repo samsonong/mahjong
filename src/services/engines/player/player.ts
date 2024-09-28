0a80f560dabc0a2bbaffcb702f67c137387bb488
Samson Ong <contact@samsonong.com>
2024-09-29 00:13:34 +0800

    Remove `POSSIBLE_ACTION`

--- src/services/engines/player/player.ts
+++ src/services/engines/player/player.ts
@@ -1,38 +1,14 @@
-import { SUITS } from "../scoring/scoring";
 import { SORTED_TILES } from "../tiles/sortTiles";
-import { TILE_TYPE } from "../tiles/tiles";
 
 export type HAND = {
   open: SORTED_TILES[];
   closed: SORTED_TILES[];
-  possibleActions: POSSIBLE_ACTIONS;
-};
-
-type POSSIBLE_ACTIONS = {
-  chi: POSSIBLE_ACTION_NODE[];
-  pon: POSSIBLE_ACTION_NODE[];
-  kan: POSSIBLE_ACTION_NODE[];
-  ron: {
-    target: TILE_TYPE["tile"][];
-    type: SUITS;
-  }[];
-};
-type POSSIBLE_ACTION_NODE = {
-  id: string;
-  targets: TILE_TYPE["tile"][];
-  base: TILE_TYPE[];
 };
 
 export class Player {
   hand: HAND = {
     open: [],
     closed: [],
-    possibleActions: {
-      chi: [],
-      pon: [],
-      kan: [],
-      ron: [],
-    },
   };
   wallet: number;
 
